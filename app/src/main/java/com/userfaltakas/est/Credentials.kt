264a22b766f5173f0e7807682ea74c2884e991d9
orestis faltakas <faltakas@ics.forth.gr>
2021-10-10 14:57:29 +0300

    Initial commit

---
+++ app/src/main/java/com/userfaltakas/est/Credentials.kt
@@ -0,0 +1,6 @@
+package com.userfaltakas.est
+
+object Credentials {
+    const val APP_ID = "userfaltakas-gmail-com-s-y-ial"
+    const val APP_TOKEN = "afc1f42210acaab449c76056cb0c53e9"
+}
---
+++ app/src/main/java/com/userfaltakas/est/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.userfaltakas.est
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory
+import com.estimote.proximity_sdk.api.*
+import com.userfaltakas.est.Credentials.APP_ID
+import com.userfaltakas.est.Credentials.APP_TOKEN
+import com.userfaltakas.est.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var proximityObserver: ProximityObserver
+    private var proximityObservationHandler: ProximityObserver.Handler? = null
+
+    private val cloudCredentials = EstimoteCloudCredentials(
+        APP_ID,
+        APP_TOKEN
+    )
+
+    // Lambda functions for displaying errors when checking requirements
+    private val displayToastAboutMissingRequirements: (List<Requirement>) -> Unit = {
+        Toast.makeText(
+            this,
+            "Unable to start proximity observation. Requirements not fulfilled: ${it.size}",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+    private val displayToastAboutError: (Throwable) -> Unit = {
+        Toast.makeText(
+            this,
+            "Error while trying to start proximity observation: ${it.message}",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Requirements check
+        RequirementsWizardFactory.createEstimoteRequirementsWizard().fulfillRequirements(
+            this,
+            onRequirementsFulfilled = { startProximityObservation() },
+            onRequirementsMissing = displayToastAboutMissingRequirements,
+            onError = displayToastAboutError
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        proximityObservationHandler?.stop()
+    }
+
+    private fun startProximityObservation() {
+        proximityObserver = ProximityObserverBuilder(applicationContext, cloudCredentials)
+            .onError(displayToastAboutError)
+            .withTelemetryReportingDisabled()
+            .withAnalyticsReportingDisabled()
+            .withEstimoteSecureMonitoringDisabled()
+            .withBalancedPowerMode()
+            .build()
+
+        val beacons = ArrayList<ProximityZone>()
+        beacons.add(zoneBuild("Green"))
+        beacons.add(zoneBuild("Blue"))
+        beacons.add(zoneBuild("White"))
+        beacons.add(zoneBuild("Purple"))
+        proximityObservationHandler = proximityObserver.startObserving(beacons)
+    }
+
+    private fun zoneBuild(tag: String): ProximityZone {
+        return ProximityZoneBuilder()
+            .forTag(tag)
+            .inNearRange()
+            .onEnter {
+                binding.textView.text = "Enter: ${it.tag}"
+            }
+            .onExit {
+                binding.textView.text = "Exit: ${it.tag}"
+            }
+            .build()
+    }
+}
